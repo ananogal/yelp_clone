47e415e58de140c0909c5cbe7fdb1fc9d57450f5
ananogal <ana.nogal@gmail.com>
2014-11-03 14:02:17 +0000

    Create a Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,13 @@ class RestaurantsController < ApplicationController
 	def index
 		@restaurants = Restaurant.all
 	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.create(params[:restaurant].permit(:name))
+		redirect_to '/restaurants'
+	end
 end
